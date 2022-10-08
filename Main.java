cbcf7ea4022b50f0c53af7019d2e27622b95a156
visabella <111545124+visabella@users.noreply.github.com>
2022-10-08 20:36:07 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int numero, num;
+    System.out.println("Informe o numero que deseja: ");
+    numero = scan.nextInt();
+
+    for ( num = 1; num <= numero; num++) {
+   if ((num%2) == 1) {
+    System.out.println(num);
+   }
+  }
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
+  }
+}
